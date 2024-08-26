aa6da2be4b8023749441a9a178fa37a4cc30a97e
Sunil <sunil.nunubilli@gmail.com>
2024-08-26 22:19:22 +0530

    Created file 04

--- 06-Array.sh
+++ 06-Array.sh
@@ -2,9 +2,9 @@
 
 Persons=("Darshana" "Jai" "Sunil" "Bhuvnaa")
 
-echo "$Persons[0]"
-echo "$Persons[1]"
-echo "$Persons[2]"
-echo "$Persons[3]"
+echo "${Persons[0]}"
+echo "${Persons[1]}"
+echo "${Persons[2]}"
+echo "${Persons[3]}"
 
-echo "All the persons in the famil are $Persons[@]"
+echo "All the persons in the famil are ${Persons[@]}"
