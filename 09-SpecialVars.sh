4980073bb10c3a99e24b9bf6a46d41e7c7c42645
Sunil <sunil.nunubilli@gmail.com>
2024-08-26 22:48:16 +0530

    Created file 04

--- 09-SpecialVars.sh
+++ 09-SpecialVars.sh
@@ -1,3 +1,6 @@
 echo "the varibales that are passed from are $@"
 echo "No of variables passed are $#"
 echo "the script that is being executed is $0"
+echo "Current dorectory : $PWD"
+echo "home directory of current user: $HOME"
+echo "PID of the current commnd : $$"
