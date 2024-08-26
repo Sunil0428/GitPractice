e57aa498f05b401b81176516a2dab2a6e6fd9c87
Sunil <sunil.nunubilli@gmail.com>
2024-08-26 21:22:56 +0530

    I did few changes in the file

---
+++ 01-hello-word.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Hi My name is Sunil and this is my first shell script
+#to set up git hub , first we need to crate an account with git hub
+#Then we need to clone that created Repository in our locla system folder
+#then we need to access that folder using gitbash
+#using VScode editor we can write scripts in that folder 
