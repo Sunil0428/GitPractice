3674bb4a55af0b26a0b936862487e4275bba06d9
Sunil <sunil.nunubilli@gmail.com>
2024-08-27 13:04:20 +0530

    Created file 04

--- 12-Loop.sh
+++ 12-Loop.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-for i in {1..1000}
-do
- echo $i
 
+USER=$(id -u)
+R="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT()
+{
+    if [ $USER -ne 0 ]
+    then
+     echo  -e "you cant install as $R you dnt have req priviliges $N" 
+     exit 1
+     fi
+}
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$R $2 is SUCCESS $N"
+    else 
+        echo -e "$Y $2 is not FAILED $N"
+    fi
+}
+CHECK_ROOT
+
+for PACKAGE in $@ #this referes all te arguments passed to this file at run time
+do 
+echo $PACKAGE
 done
