e7f35d77c129214590f52825a2f2687c3ee28bec
Sunil <sunil.nunubilli@gmail.com>
2024-08-27 11:15:02 +0530

    Created file 04

--- 11-Installation.sh
+++ 11-Installation.sh
@@ -13,12 +13,10 @@ then
    echo "you cant install as you dnt have req priviliges" 
    exit 1
 fi
-
-
  dnf list installed git 
-
  VALIDATE $?
 
+ 
 # if [ $? -eq 0 ]
 # then 
 #      echo "Git is already installed"
