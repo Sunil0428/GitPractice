ef0ffd351a1722b4a45c6c5abf6101c5b5774472
Sunil <sunil.nunubilli@gmail.com>
2024-08-27 11:15:51 +0530

    Created file 04

--- 11-Installation.sh
+++ 11-Installation.sh
@@ -15,34 +15,3 @@ then
 fi
  dnf list installed git 
  VALIDATE $?
-
- 
-# if [ $? -eq 0 ]
-# then 
-#      echo "Git is already installed"
-# else
-#       echo "Git is not yet installed"
-#       dnf install git -y
-#       if [ $? -ne 0 ]
-#       then  
-#            echo "Git Insatllation is failed .... please check"
-#       else
-#            echo "Git Installation is successful"
-#       fi        
-# fi
-
-# dnf list installed mysql 
-
-# if [ $? -eq 0 ]
-# then 
-#      echo "mysql is already installed"
-# else
-#       echo "mysql is not yet installed"
-#       dnf install mysql -y
-#       if [ $? -ne 0 ]
-#       then  
-#            echo "mysql Insatllation is failed .... please check"
-#       else
-#            echo "mysql Installation is successful"
-#       fi      
-# fi
