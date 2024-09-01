82a0ce6781d8a80cceae9ba65f6f50d68daa26a2
Sunil <sunil.nunubilli@gmail.com>
2024-09-01 22:05:34 +0530

    Created

---
+++ 14-LogsCleanUp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOGSFOLDER="/home/ec2-user/logs"
+
+R="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d "$LOGSFOLDER" ]
+{
+    Then
+        echo -e "$Y$LOGSFOLDER exists $N"
+    else
+        echo -e "$R$LOGSFOLDER is not exists $N"
+}
