a2fb6190248a722ef5a482efb7f11d30371542b8
Sunil <sunil.nunubilli@gmail.com>
2024-09-01 22:28:23 +0530

    Created

--- 14-LogsCleanUp.sh
+++ 14-LogsCleanUp.sh
@@ -13,6 +13,6 @@ if [ -d "$LOGSFOLDER" ]
         echo -e "$R $LOGSFOLDER is not exists $N"
 fi
 
-Files=$(find $LOGSFOLDER -name *.log -mtime 30)
+Files=$(find $LOGSFOLDER -name *.log -mtime +30)
 
 echo $Files
