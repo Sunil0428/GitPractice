1ccbfe56aadd87ae098a2c0401754c4bde7179ec
Sunil <sunil.nunubilli@gmail.com>
2024-09-02 13:36:44 +0530

    Created

---
+++ 15-Backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SOURCEDIR=$1
+DESTFIR=$2
+FILEOLDERTHAN=$3
+
+R="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USAGE()
+{
+    echo -e "$Y Bachup.sh <SourceDir> <DestDir> <No of days Files Older than>"
+}
+
+if[ $2 -lt 2 ]
+then 
+    USAGE
+fi
